e685105e74965da2e8d441e8a08a3ed5e9b04165
danos <danos_p@example.com>
2021-03-02 22:19:03 +0100

    License header update email.

--- src/main/java/info/danos/Main.java
+++ src/main/java/info/danos/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Damian Duda <damian.duda@gmail.com>
+ * Copyright (c) 2020, Damian Duda <danos_p@poczta.onet.pl>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
